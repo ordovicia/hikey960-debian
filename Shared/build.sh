42d9eb332e14ef9aa4446fe01c3ace183c3556b6
Hidehito Yabuuchi <hdht.ybuc@gmail.com>
2018-01-16 21:15:12 +0900

    Separate home directory image

--- Shared/build.sh
+++ Shared/build.sh
@@ -108,8 +108,7 @@ setup_wifi() { (
     sudo mkdir -p rootfs/lib/firmware/ti-connectivity
     sudo cp wl18xx-fw-4.bin rootfs/lib/firmware/ti-connectivity/
 
-    cat >> rootfs/etc/network/interfaces << EOF
-
+    cat << EOF | sudo tee -a rootfs/etc/network/interfaces
 auto lo wlan0
 iface lo inet loopback
 allow-hotplug wlan0
@@ -149,29 +148,49 @@ EOF
 create_sparce_rootfs_image() { (
     cd Build
 
-    dd if=/dev/zero of=rootfs.img bs=1M count=4096
+    sudo mv rootfs/home .
+
+    cat << EOF | sudo tee -a rootfs/etc/fstab
+/dev/sdd10  /      ext4 defaults,noatime    0   1
+/dev/sdd13  /home  ext4 defaults,noatime    0   2
+EOF
+
+    dd if=/dev/zero of=rootfs.img bs=1M count=4688
     mkfs.ext4 -F -L rootfs rootfs.img
     sudo mount -o loop rootfs.img loop
-
     (
         cd rootfs
         sudo tar -cf - * | ( cd ../loop; sudo tar -xf - )
     )
-
     sudo umount loop
     img2simg rootfs.img rootfs.sparse.img 4096
+
+    dd if=/dev/zero of=home.img bs=1M count=2048
+    sudo mkfs.ext4 -F home.img
+    sudo mount -o loop home.img loop
+    (
+        cd home
+        sudo tar -cf - * | ( cd ../loop; sudo tar -xf - )
+    )
+    sudo umount loop
+    img2simg home.img home.sparse.img 4096
 ) }
 
 flash() { (
-    cd Build
+    dir=Shared/$(date "+%Y%m%d-%H%M%S")
+    mkdir $dir
 
-    # sudo fastboot flash boot boot-fat.uefi.img
-    # sudo fastboot flash system rootfs.sparse.img
+    cat << EOF
+Please flash images manually:
+$ cd $dir
+$ sudo fastboot flash boot boot-fat.uefi.img
+$ sudo fastboot flash system rootfs.uefi.img
+$ sudo fastboot flash userdata home.uefi.img
+EOF
 
-    echo "Please flash boot-fat.uefi.img and rootfs.sparse.img manually"
-    date=$(date "+%Y%m%d-%H%M%S")
-    cp boot-fat.uefi.img ~/Shared/boot-fat-$date.uefi.img
-    cp rootfs.sparse.img ~/Shared/rootfs-$date.sparse.img
+    mv Build/boot-fat.uefi.img $dir/boot-fat.uefi.img
+    mv Build/rootfs.sparse.img $dir/rootfs.sparse.img
+    mv Build/home.sparse.img   $dir/home.sparse.img
 ) }
 
 mkdir -p Downloads Build/loop
